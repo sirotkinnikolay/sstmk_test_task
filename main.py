65d939f7a3c67cab1c83a29204bc2969c0c63142
sirotkinnikolay <95248794+sirotkinnikolay@users.noreply.github.com>
2024-02-29 12:36:52 +0300

    Update main.py
    Add phone company search

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import socket
 
 
 class UrlPage:
-    def __init__(self, url, s_pattern, host_n):
+    def __init__(self, url, s_pattern, host_n, phone_company_pattern):
         self.s_pattern = s_pattern
         self.host_n = host_n
         self.response = requests.get(url)
+        self.phone_company_pattern = phone_company_pattern
 
     def page(self):
         if self.response.status_code == 200:
@@ -26,16 +27,21 @@ class UrlPage:
         else:
             number_list = [number for number in phone_numbers]
             return f'Найдены номера телефона, подходящие шаблону поиска: {number_list}'
+    def company_phone_number(self):
+        phone_company = re.findall(self.phone_company_pattern, self.response.text)[0][4:]
+        return f'На странице найден телефон компании: {phone_company}'
 
 
 if __name__ == '__main__':
     page_url = "http://sstmk.ru"
     search_patter = r'(\+d{1,3}\(d+\)d+-d+-d+|\(d+\)d+-d+-d+)'
+    phone_company_pat = r'tel:\d+'
     host_name = "sstmk.ru"
     try:
-        instance = UrlPage(page_url, search_patter, host_name)
+        instance = UrlPage(page_url, search_patter, host_name, phone_company_pat)
         print(instance.page())
         print(instance.ip_address())
         print(instance.search_phone_number())
+        print(instance.company_phone_number())
     except Exception as e:
         print(f'Ошибка: {type(e).__name__}')
